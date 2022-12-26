62c4d9b326d65684a67cdb8d30b5ea5f6359eb69
marcos echeverria <marcosalbertoecheverria@gmail.com>
2022-12-26 11:03:43 -0300

    deploy

--- djangoProject/ecomerce/ecomerce/settings.py
+++ djangoProject/ecomerce/ecomerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2)(io$bu$e4u#7$u4d_k_m!6tct#df9$nt4&zux#jha2==9nlb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
