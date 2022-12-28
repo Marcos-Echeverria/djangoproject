0e82794c8e208284be3fa153374b1a2cb04474a9
marcos echeverria <marcosalbertoecheverria@gmail.com>
2022-12-28 12:38:09 -0300

    refactorizando

--- djangoProject/README.md
+++
@@ -1,4 +0,0 @@
-# djangoProject
-Grupo 4
-Integrantes:
-Lucia del Valle Ledezma, Ramiro J. Súarez Paz, Marcos Echecverria, Soledad Zambrano
--- djangoProject/ecomerce/comerce/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComerceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'comerce'
