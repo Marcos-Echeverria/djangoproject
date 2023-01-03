a3508b558534e698d1dc7b3b702435f89c1060b1
marcos echeverria <marcosalbertoecheverria@gmail.com>
2023-01-03 09:00:32 -0300

    collector

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -122,11 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-#BASE_DIR = 'djangoProject/ecomerce/comerce/static'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFIELDS_DIR = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
